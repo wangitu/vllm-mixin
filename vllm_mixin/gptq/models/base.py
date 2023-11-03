d607d66ca5e5ec59e77fbb456bbd993800ea91c7
wangitu <wql20000111@gmail.com>
2023-11-03 12:44:44 +0000

    add baichuan gptq support

--- vllm_mixin/gptq/models/base.py
+++ vllm_mixin/gptq/models/base.py
@@ -6,7 +6,11 @@ from vllm.model_executor.parallel_utils.parallel_state import (
 )
 
 
+def _verify_parallel():
+    tensor_model_parallel_world_size = get_tensor_model_parallel_world_size()
+    assert tensor_model_parallel_world_size == 1, "Tensor model parallel is not compatible with GPTQ, please set `tensor_parallel_size = 1`"
+
+
 class GPTQForCausalLM:
     def __init__(self):
-        tensor_model_parallel_world_size = get_tensor_model_parallel_world_size()
-        assert tensor_model_parallel_world_size == 1, "Tensor model parallel is not compatible with GPTQ, please set `tensor_parallel_size = 1`"
+        _verify_parallel()
